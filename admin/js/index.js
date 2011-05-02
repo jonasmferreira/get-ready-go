4c1c1a9bbb6141fcca5b507e33a986abdef21093
inboxfox@gmail.com <inboxfox@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-05-02 12:51:31 +0000

    Correções

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,24 +1,11 @@
-var erros = new Array();
-erros[0] = "Login/Senha incorretos";
-erros[1] = "Preencha um Valor Válido";
-
 (function($){
 	$(document).ready(function(){
-		jQuery("#erro_logon").css({
-			'display':'none'
-		});
 		$("#enviar").click(function(e){
 			e.preventDefault();
-			$("#erro_logon").css({
-				'display':'none'
-			});
-			if(!verifyObrigatory()){
-				$("#erro_logon").css({
-					'display':'block'
-				});
-				$("#erro_logon_message span").html(erros[1]);
+			if(verifyObrigatorio()){
+				$("#frmlogin").submit();
 			}else{
-				newAlert("opa");
+				newAlert("Preencha os campos em destaque");
 			}
 		});
 	});
