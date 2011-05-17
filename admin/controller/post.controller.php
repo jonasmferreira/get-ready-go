2c4dd6e320ba00f8f60968fac2b2eadfbd342c3a
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-05-17 01:20:42 +0000

    Alterações no controller para colocar novas opções de Ajax

---
+++ admin/controller/post.controller.php
@@ -0,0 +1,47 @@
+<?php
+
+	$path_root_postController = dirname(__FILE__);
+	$DS = DIRECTORY_SEPARATOR;
+	$path_root_postController = "{$path_root_postController}{$DS}..{$DS}..{$DS}";
+	require_once "{$path_root_postController}admin{$DS}model{$DS}post.class.php";
+	$obj = new post();
+	switch($_REQUEST['action']){
+		case 'getLista':
+		default:
+			$obj->setLimitStart($_REQUEST['start']);
+			$obj->setLimitMax($_REQUEST['limit']);
+			$obj->setSort($_REQUEST['sort']);
+			
+			$obj->setValues($_REQUEST);
+			
+			$aJson = array();
+			$aJson = $obj->getLista();
+			echo json_encode($aJson);
+		break;
+		case 'getCategoria':
+			$aJson = array();
+			$aJson = $obj->getCategoria();
+			echo json_encode($aJson);
+		break;
+		case 'getUsuario':
+			$aJson = array();
+			$aJson = $obj->getUsuario();
+			echo json_encode($aJson);
+		break;
+	
+		case 'edit':
+			$obj->setValues($_REQUEST);
+			//$obj->setFiles($_FILES);
+			$result = $obj->edit();
+			if($result['success']===false){
+				$obj->setSession('msgEdit', 'Erro ao tentar criar/editar Post!');
+				header('Location: ../post.php');
+			}else{
+				$obj->setSession('msgEdit', 'Post salvo com Sucesso!');
+				header('Location: ../postEdicao.php?post_id='.$result['post_id']);
+			}
+		break;
+	}
+	
+	
+?>
