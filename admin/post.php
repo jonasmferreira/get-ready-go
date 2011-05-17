80e44feb9f00b1c36d79b61a0af33d1fc293212c
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-05-17 01:22:49 +0000

    POST

---
+++ admin/post.php
@@ -0,0 +1,22 @@
+<?php
+	$path_root_postView = dirname(__FILE__);
+	$DS = DIRECTORY_SEPARATOR;
+	$path_root_postView = "{$path_root_postView}{$DS}..{$DS}";
+	include_once("{$path_root_postView}admin{$DS}includes{$DS}header.php");
+	include_once("{$path_root_postView}admin{$DS}model{$DS}post.class.php");
+	$objPost = new post();
+	if(isset($session['msgEdit'])&&trim($session['msgEdit'])!=''){
+		echo "
+			<script type=\"text/javascript\">
+				newAlert('{$session['msgEdit']}');
+			</script>
+		";
+	}
+	$obj->unsetSession('msgEdit');
+?>
+<!-- js admin include -->
+<script type="text/javascript" src="js/post.js"></script>
+<div id="grid"></div>
+<?	
+	include_once("{$path_root_postView}admin{$DS}includes{$DS}footer.php");
+?>
