8fce52106e5a1502b02c760c18c4051e073560d0
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-05-17 01:26:11 +0000

    Script de edição de POST, em construção ainda

---
+++ admin/js/postEdicao.js
@@ -0,0 +1,20 @@
+(function($){
+	$(document).ready(function(){
+		$("#salvar").click(function(){
+			if(!verifyObrigatorio()){
+				newAlert("Campos marcados são obrigatórios");
+			}else{
+				$("#formSalvar").submit();
+			}
+		});
+		$("#limparCadastro").click(function(){
+			$("#formSalvar input[type=text], #formSalvar input[type=password]").val('');
+			$("#formSalvar select option:first").attr('selected','selected');
+		});
+		
+		$("#voltar").click(function(){
+			window.location.href = 'usuario.php';
+		});
+		
+	});
+})(jQuery);
