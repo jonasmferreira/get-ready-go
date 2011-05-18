76e5a3ba8187ba7d60463a4b3cbe5e03b6a943e0
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-05-18 20:33:27 +0000

    Adicionado CKEDITOR

--- admin/js/ckeditor/config.js
+++ admin/js/ckeditor/config.js
@@ -8,4 +8,12 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	
+	config.filebrowserBrowseUrl = '../admin/js/ckfinder/ckfinder.html',
+	config.filebrowserImageBrowseUrl = '../admin/js/ckfinder/ckfinder.html?type=Images',
+	config.filebrowserFlashBrowseUrl = '../admin/js/ckfinder/ckfinder.html?type=Flash',
+	config.filebrowserUploadUrl = '../admin/js/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
+	config.filebrowserImageUploadUrl = '../admin/js/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
+	config.filebrowserFlashUploadUrl = '../admin/js/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
+	
 };
