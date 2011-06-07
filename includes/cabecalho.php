82ad358b1ce3c23f0977f527613297de499917bc
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-06-07 03:42:15 +0000

    Script para carregar propaganda em flash!

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -5,7 +5,8 @@
 	}
 	//echo "<pre>".print_r($_SESSION,true)."</pre>";
 	//echo "<pre>".print_r($_SERVER,true)."</pre>";
-	$linkAbsolute = "http://localhost/get/";
+	$linkAbsolute = "http://localhost/GetReadyGo/";
+	//$linkAbsolute = "http://getreadygo.cdanime.com/";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -15,7 +16,10 @@
 		<title>Get Ready... Go!</title>
 		<link href="<?php echo $linkAbsolute?>style.css" media="screen" rel="stylesheet" type="text/css" />
 		<script type="text/javascript" src="<?php echo $linkAbsolute?>js/jquery.js"></script>
-		<script type="text/javascript" src="<?php echo $linkAbsolute?>js/login.js"></script>
+		<script type="text/javascript" src="<?php echo $linkAbsolute?>js/basic.js"></script>
+		
+		
+		
 	</head>
 	<body>
 	<input type="hidden" id="linkAbsolute" name="linkAbsolute" value="<?php echo $linkAbsolute?>" />
