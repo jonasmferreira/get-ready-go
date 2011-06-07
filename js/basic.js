bfa4de4e7ad88db7bbb33d9446748e315bc8a855
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-06-07 03:39:20 +0000

    Script para carregar propaganda em flash!

---
+++ js/basic.js
@@ -0,0 +1,10 @@
+function buildFlash(swfLocation,x,y,swfMode, variaveis){
+	var swfLocation, x, y, swfMode, variaveis;
+	if(variaveis == undefined || variaveis.length < 1) variaveis = '';
+	document.write(' <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="'+x+'" height="'+y+'">');
+	document.write(' <param name="movie" value="'+swfLocation+variaveis+'" />');
+	document.write(' <param name="quality" value="best" />');
+	document.write(' <param name="wmode" value="'+swfMode+'" />');
+	document.write(' <embed src="'+swfLocation+variaveis+'" wmode="'+swfMode+'" quality="best" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="'+x+'" height="'+y+'"></embed>');
+	document.write(' </object>');
+}
