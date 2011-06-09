975ac597d5ecc46e355eeda32c420a2ee01dffdf
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-06-09 22:38:59 +0000

    Adicionado resultado de ENQUETE

--- ajax/home.php
+++ ajax/home.php
@@ -8,9 +8,37 @@ switch($_REQUEST['action']){
 	case 'addEnquete':
 		$obj->setOpcao_enquete_opcao_id($_POST['opcao_voto']);
 		if(!$obj->verifyVotos()){
-			$aResult['success'] = true;
-			$aResult['message'] = "Seu voto já foi computado!";
-			echo json_encode($aResult);
+			
+			$aEnqRes = $obj->getEnqueteResult($_POST['enquete_id']);
+			
+			$aEnqRes['success'] = true;
+			$aEnqRes['message'] = "Seu voto já foi computado!";
+			$li = '';
+			
+			$tot = 0;
+			//totalizando
+			foreach($aEnqRes['data'] as $k => $v){
+				$tot += $v['total'];
+			}
+			
+			foreach($aEnqRes['data'] as $k => $v){
+				if($v['total']==0){
+					$w = 1;
+					$per = 0;
+				}else{
+					$per = (100 * $v['total'] / $tot);
+					$w = (210 / $per * 100);
+				}
+				
+				$li.= "<li>";
+				$li.= $v['enquete_opcao_titulo'] . "<br />";
+				$li.= "<img src='{$linkAbsolute}imgs/enquete_result1.gif' />";
+				$li.= "&nbsp;<img src='{$linkAbsolute}imgs/enquete_result3.gif' width='$w' height='5' />";
+				$li.= "&nbsp;<img src='{$linkAbsolute}imgs/enquete_result2.gif' />&nbsp;" . $per ."%";
+				$li.= "</li> ";
+			}
+			$aEnqRes['result'] = $li;
+			echo json_encode($aEnqRes);
 			break;
 		}
 		if($obj->saveVoto()==false){
