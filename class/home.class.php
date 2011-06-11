3ea173b742c038a6ec1c296eafdd9519e6f4365a
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-06-11 02:01:59 +0000

    corrigido erro de retorno de resultado de enquete

--- class/home.class.php
+++ class/home.class.php
@@ -183,7 +183,7 @@ class home extends defaultClass{
 		return false;
 	}
 	
-	public function getEnqueteResult(){
+	public function getEnqueteResult($enquete_id){
 		$sql = array();
 		$sql[] = "
 			SELECT	e.enquete_id
@@ -198,7 +198,7 @@ class home extends defaultClass{
 			ON		eo.enquete_id = e.enquete_id
 
 			WHERE	0=0
-			AND		e.enquete_id = 1
+			AND		e.enquete_id = $enquete_id
 
 			GROUP BY 
 					e.enquete_id, eo.enquete_opcao_id
@@ -228,6 +228,65 @@ class home extends defaultClass{
 
 		return $rsEnq;
 	}
+
+	public function getOutdoorDestaque(){
+		$sql = array();
+		$sql[] = "
+			SELECT	p.post_id
+					,p.categoria_id
+					,c.categoria_nome
+					,p.usuario_id
+					,p.post_titulo
+					,p.post_thumb_home
+					,p.post_imagem
+					,p.post_thumb_imagem
+					,p.post_palavra_chave
+					,p.post_conteudo
+					,DATE_FORMAT(p.post_dtcomp_criacao,'%d.%m.%Y %h:%i') AS post_dt_criacao
+					,p.post_dtcomp_criacao
+					,p.post_dt_alteracao
+					,p.post_dtcomp_alteracao
+					,p.post_status
+					,u.usuario_nome
+					,u.usuario_avatar
+					,(SELECT COUNT(*) FROM tb_comentario WHERE post_id = p.post_id AND comentario_status = 1) as qtdComentario
+			FROM	tb_post p
+
+			JOIN	tb_usuario u
+			ON		p.usuario_id = u.usuario_id
+
+			JOIN	tb_categoria c
+			ON		p.categoria_id = c.categoria_id
+
+			WHERE	1 = 1
+			AND		p.post_status = 1
+			AND		c.categoria_id = {$this->categoria_id}
+			ORDER BY post_dtcomp_criacao DESC
+		";
+		$res = array();
+		$this->setTotal($this->getMaxCount(implode("\n",$sql)));
+		//echo $this->getTotal();
+		if(isset($this->limit_start)&&trim($this->limit_start)!=''){
+			$sql[] = "LIMIT {$this->limit_start}, {$this->limit_max}";
+		}
+		$result = $this->dbConn->db_query(implode("\n",$sql));
+		if(!$result['success']){
+			return false;
+		}
+		
+		if($result['total'] > 0){
+			while($rs = $this->dbConn->db_fetch_assoc($result['result'])){
+				$cat_nome = str_replace(' ', '_',$this->retiraAcentos($rs['categoria_nome']));
+				$post_nome = str_replace(' ', '+', $this->retiraAcentos($rs['post_titulo']));
+
+				$rs['linkDetalhe'] = "detalhe/{$cat_nome}/{$rs['categoria_id']}/{$post_nome}/{$rs['post_id']}";
+
+				array_push($res, $rs);
+			}
+		}
+		return $this->utf8_array_encode($res);
+	}
+	
 	
 }
 ?>
