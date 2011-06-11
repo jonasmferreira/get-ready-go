4a7d4cea808676beb2231c58a891bcf97710bc71
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-06-11 18:33:37 +0000

    Corrigido layout e adicionado opção de CANCELAR comentario

--- admin/comentarioView.php
+++ admin/comentarioView.php
@@ -27,6 +27,24 @@
 
 <!-- js admin include -->
 <script type="text/javascript" src="js/comentarioView.js"></script>
+
+<style type="text/css">
+	.contentValue{
+		border: 1px solid #CDCDCD;
+		background:#FFF;
+		padding: 2px;
+	}
+	.comentValue{
+		border: 1px solid #CDCDCD;
+		background:#FFF;
+		padding: 2px;
+		height: 200px;
+		width: 600px;
+		overflow: auto;
+	}
+	
+</style>
+
 <div class="form-main">
 	<div class="legend" >Ativação/Visualização do Comentário</div>
 	<div class="forms cadastros">
@@ -34,37 +52,39 @@
 			<input type="hidden" name="comentario_id" id="comentario_id" value="<?php echo $res['comentario_id']; ?>" />
 			<ul>
 				<li>Titulo Post:</li>
-				<li><b><?=$res['post_titulo']?></b></li>
+				<li><b class="contentValue"><?=$res['post_titulo']?></b></li>
 			</ul>
 			<ul>
 				<li>Nome do autor:</li>
-				<li><b><?=$res['comentario_autor']?></b></li>
+				<li><b class="contentValue"><?=$res['comentario_autor']?></b></li>
 			</ul>
 			<ul>
 				<li>Email do autor:</li>
-				<li><b><?=$res['comentario_email']?></b></li>
-			</ul>
-			<br clear="all" />
-			<ul>
-				<li>Comentario:</li>
-				<li><b><?=$res['comentario_conteudo']?></b></li>
+				<li><b class="contentValue"><?=$res['comentario_email']?></b></li>
 			</ul>
 			<br clear="all" />
 			<ul>
 				<li>Data Criação do Comentario:</li>
-				<li><b><?=$objcomentario->dateDB2BR($res['comentario_dt_criacao'])?></b></li>
+				<li><b class="contentValue"><?=$objcomentario->dateDB2BR($res['comentario_dt_criacao'])?></b></li>
 			</ul>
 			<ul>
 				<li>Dt/Hr Criação do Comentario:</li>
-				<li><b><?=$objcomentario->dateDB2BR($res['comentario_dtcomp_criacao'])?></b></li>
+				<li><b class="contentValue"><?=$objcomentario->dateDB2BR($res['comentario_dtcomp_criacao'])?></b></li>
 			</ul>
 			<ul>
 				<li>Data Ativação do Comentario:</li>
-				<li><b><?=$objcomentario->dateDB2BR($res['comentario_dt_alteracao'])?></b></li>
+				<li><b class="contentValue"><?=$objcomentario->dateDB2BR($res['comentario_dt_alteracao'])?></b></li>
 			</ul>
 			<ul>
 				<li>Dt/Hr Ativação do Comentario:</li>
-				<li><b><?=$objcomentario->dateDB2BR($res['comentario_dtcomp_alteracao'])?></b></li>
+				<li><b class="contentValue"><?=$objcomentario->dateDB2BR($res['comentario_dtcomp_alteracao'])?></b></li>
+			</ul>
+			<br clear="all" />
+			<ul>
+				<li>Comentario:</li>
+				<li>
+					<textarea class="comentValue"><?=$res['comentario_conteudo']?></textarea>
+				</li>
 			</ul>
 			<br clear="all"/>
 			<ul style="width:300px">
@@ -72,8 +92,9 @@
 				<li>
 					<select id="comentario_status" name="comentario_status" class="obrigatorio">
 						<option value="">Selecione um Status</option>
-						<option value="1" <?=($res['comentario_status']=='1'?' selected="selected"':'')?>>Ativo</option>
-						<option value="0" <?=($res['comentario_status']=='0'?' selected="selected"':'')?>>Inativo</option>
+						<option value="1" <?=($res['comentario_status']=='1'?' selected="selected"':'')?>>Liberado</option>
+						<option value="0" <?=($res['comentario_status']=='0'?' selected="selected"':'')?>>Não Liberado</option>
+						<option value="2" <?=($res['comentario_status']=='2'?' selected="selected"':'')?>>Cancelado</option>
 					</select>
 				</li>
 			</ul>
