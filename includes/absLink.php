b874be9c89d2b2b9713d76e6360e35dfeb4eafc7
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-06-16 14:41:31 +0000

    Adicionado link absoluto pro site definitivo

--- includes/absLink.php
+++ includes/absLink.php
@@ -1,7 +1,8 @@
 <?php
 
 	//$linkAbsolute = "http://localhost/get/";
-	$linkAbsolute = "http://localhost/GetReadyGo/";
+	//$linkAbsolute = "http://localhost/GetReadyGo/";
 	//$linkAbsolute = "http://getreadygo.cdanime.com/";
+	$linkAbsolute = "http://getready.net.br/";
 	
 ?>
