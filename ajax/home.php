7313dee57e75892071728663ce74d355e271c9e3
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-06-16 15:45:47 +0000

    Corrigido problema da ENQUETE no primeiro VOTO

--- ajax/home.php
+++ ajax/home.php
@@ -8,47 +8,18 @@ switch($_REQUEST['action']){
 	case 'addEnquete':
 		$obj->setOpcao_enquete_opcao_id($_POST['opcao_voto']);
 		if(!$obj->verifyVotos()){
-			
 			$aEnqRes = $obj->getEnqueteResult($_POST['enquete_id']);
-			
-			$aEnqRes['success'] = true;
 			$aEnqRes['message'] = "Seu voto já foi computado!";
-			$li = '';
-			
-			//totalizando
-			$tot = 0;
-			foreach($aEnqRes['data'] as $k => $v){
-				$tot += $v['total'];
-			}
-			
-			foreach($aEnqRes['data'] as $k => $v){
-				if($v['total']==0){
-					$w = 0;
-					$per = 0;
-				}else{
-					$per = (100 * $v['total'] / $tot);
-					$w = (210 / 100 * $per);
-				}
-				$per = number_format($per,2);
-				$w = number_format($w,0);
-				
-				$li.= "<li>";
-				$li.= $v['enquete_opcao_titulo'] . "<br />";
-				$li.= "<img src='{$linkAbsolute}imgs/enquete_result1.gif' /><img src='{$linkAbsolute}imgs/enquete_result3.gif' width='$w' height='5' /><img src='{$linkAbsolute}imgs/enquete_result2.gif' />&nbsp;" . $per ."%";
-				$li.= "</li> ";
-			}
-			$aEnqRes['result'] = $li;
-			echo json_encode($aEnqRes);
-			break;
-		}
-		if($obj->saveVoto()==false){
-			$aResult['success'] = false;
-			$aResult['message'] = "Error ao computar o seu voto";
 		}else{
-			$aResult['success'] = true;
-			$aResult['message'] = "Seu voto foi computado com sucesso!";
+			if($obj->saveVoto()==false){
+				$aEnqRes['success'] = false;
+				$aEnqRes['message'] = "Error ao computar o seu voto";
+			}else{
+				$aEnqRes = $obj->getEnqueteResult($_POST['enquete_id']);
+				$aEnqRes['message'] = "Seu voto foi computado com sucesso!";
+			}
 		}
-		echo json_encode($aResult);
+		echo json_encode($aEnqRes);
 	break;
 	case 'rss_noticias':
 		// Define o tipo de conteúdo e o charset
