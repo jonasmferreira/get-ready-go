6ce9da38f024efde4e5a89aa919eed5cf1a0fdb2
joynilson@gmail.com <joynilson@gmail.com@6bdb7b0f-fb7a-eea0-690c-5b13a7683822>
2011-06-19 18:19:19 +0000

    problema de bg

--- includes/footer.php
+++ includes/footer.php
@@ -4,12 +4,17 @@
 			<!-- Rodapé -->
 			<div id="rodape">
 				<div>
-					<div><a href="javascript:void(0);" id="feedRss"><img src="<?=$linkAbsolute?>imgs/feed.png" align="absmiddle" /> Feed</a></div>
+					<div>
+						<a href="javascript:void(0);" id="feedRss"><img src="<?=$linkAbsolute?>imgs/feed.png" align="absmiddle" /> Feed</a>
+					</div>
 					<div align="right">Copyright © 2011 Get Ready... Go!. Todos os direitos reservados.</div>
 					<br clear="all" />
 				</div>
+				<div style="clear:both;"></div>
 			</div>
+			<div style="clear:both;"></div>
 		</div>
+		<div style="clear:both;"></div>
 	</div>
 </body>
 </html>
